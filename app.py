eea5bdfaa1db67475675d7cbc23e20546ff47234
German Portes <germanportes@Germans-MacBook-Air.local>
2019-10-17 00:37:50 -0400

    final changes

--- app.py
+++ app.py
@@ -10,63 +10,124 @@ import pickle
 app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 app.title='Stock prices'
-
+colors = {
+    'background': '#111111',
+    'text': '#FDFEFE'
+}
 ########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1('Nats gonna win'),
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+#app.layout = html.Div(children=[
+    html.H1(
+        children = 'Stock Closing Prices',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
     html.Div([
-    html.H6('Stop Open'),
+    html.H6('LOW'),
             dcc.Slider(
                 id ='slider1',
-                min=1,
-                max=8,
+                min=0,
+                max=10,
                 step=0.1,
-                marks= {1:str(i) for i in range(1,9)},
-                value=5,
+                marks= {1:str(i) for i in range(1,11)},
+                value=4,
                 ),
-    html.H6('Stock Close'),
+    html.H6('HIGH'),
             dcc.Slider(
-            id ='slider2',
-            min=1,
-            max=8,
+                id ='slider2',
+                min=0,
+                max=4,
+                step=0.1,
+                marks= {1:str(i) for i in range(1,4)},
+                value=4,
+                ),
+    html.H6('OPEN'),
+            dcc.Slider(
+                id ='slider3',
+                min=0,
+                max=4,
+                step=0.1,
+                marks= {1:str(i) for i in range(1,4)},
+                value=4,
+                ),
+    html.H6('SMA_5'),
+            dcc.Slider(
+                id ='slider4',
+                min=0,
+                max=4,
+                step=0.1,
+                marks= {1:str(i) for i in range(1,4)},
+                value=4,
+                ),
+    html.H6('Volume'),
+            dcc.Slider(
+                id ='slider5',
+                min=0,
+                max=4,
+                step=0.1,
+                marks= {1:str(i) for i in range(1,4)},
+                value=4,
+                ),
+    html.H6('Daily Change'),
+            dcc.Slider(
+            id ='slider6',
+            min=0,
+            max=4,
             step=0.1,
-            marks= {1:str(i) for i in range(1,9)},
-            value=5,
+            marks= {1:str(i) for i in range(1,20)},
+            value=4,
     ),
     html.Br(),
-    html.H6('# of neighbors'),
+    #html.H6('STOCK MARKET TICKERS'),
+    html.H6(
+    children = 'STOCK MARKET TICKERS',
+    style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }
+    ),
     dcc.Dropdown(
         id= 'k-drop',
-        options =[{'label':'AAPL', 'value':5},
-                 {'label': 'F', 'value':10},
-                 {'label': 'AMD', 'value': 15},
-                 {'label': 'BAC', 'value': 20},
-                 {'label': 'GE', 'value': 20}
+        options =[{'label':'AAPL', 'value':'AAPL'},
+                 {'label': 'F', 'value':'F'},
+                 {'label': 'AMD', 'value': 'AMD'},
+                 {'label': 'BAC', 'value': 'BAC'},
+                 {'label': 'GE', 'value': 'GE'}
         ],
         #options =[{'label': i, 'value': 1} for i in [5,10,15,20,25]],
-        value=5
+        value='AAPL'
     )
     ]),
     html.Div(id='output-message'),
     html.Br(),
-    html.A('Code on Github', href='https://github.com/austinlasseter/knn_iris_plotly'),
+
+    html.A('Code on Github', href='https://github.com/gportes24/Final_project'),
 ])
 
 ### Callbacks
 @app.callback(Output('output-message', 'children'),
                 [Input('k-drop', 'value'),
                 Input('slider1', 'value'),
-                Input('slider2', 'value')
+                Input('slider2', 'value'),
+                Input('slider3', 'value'),
+                Input('slider4', 'value'),
+                Input('slider5', 'value'),
+                Input('slider6', 'value')
+
                 ])
-def my_flunky_function(k, value0, value1):
-    file = open(f'resources/_model{}.pkl', 'rb')
+def my_flunky_function(k, value1, value2, value3, value4, value5, value6):
+    file = open(f'resources/{k}_final_model.pkl', 'rb')
     model= pickle.load(file)
     file.close()
     # define the new observation from the chosen values
-    new_obs =[[value0, value1]]
+    new_obs =[[value1, value2, value3, value4, value5, value6]]
     my_prediction = model.predict(new_obs)
     #return (f' you chose {k} and {value0} and {value1}')
-    return f' you chose {} and the predicted species number is:{my_prediction}'
+    return f' you chose {k} and the predicted closing price is $:{my_prediction}'
+
 
 
 
